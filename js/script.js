030719fc482656eff048fa4e7f3cbfab70fa2a02
Glitch (rock-paper-scissors4) <none>
2021-11-18 00:48:31 +0000

    🌋🏮 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 
-console.log('hello');
+let randomNumber =0;
+
+$(".play").click(function(){
+  let userText = $("input").val();
+  $(".userChoice").text(userText);
+  
+  randomNumber = Math.random();
+  $(".computerChoice").text(randomNumber);
+  
+})
